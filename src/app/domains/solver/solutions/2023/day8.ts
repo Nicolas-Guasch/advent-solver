8662b31c0f841d0d5d0e7af64f2e150d86e3e5cb
Nicolas Guasch <nic.guasch@gmail.com>
2024-10-16 15:09:04 -0300

    Add day 8 part 2 attempt. Too slow

--- src/app/domains/solver/solutions/2023/day8.ts
+++ src/app/domains/solver/solutions/2023/day8.ts
@@ -33,15 +33,59 @@ export class Day8 extends Solution {
         .split(', ');
       this.adjacent.push([neighborLeft, neighborRight]);
     }
-    console.log(Array.from(this.nodeIndex.entries()));
-    console.log(this.adjacent);
     return this.search(
       'AAA',
       { directions: instructions, index: 0 },
       'ZZZ',
     ).toString();
   }
+
+  ghostSearch(
+    nodes: string[],
+    instructions: { directions: string; index: number },
+  ): number {
+    let goalReached = false;
+
+    while (!goalReached) {
+      goalReached = true;
+      const next =
+        instructions.directions[
+          instructions.index % instructions.directions.length
+        ] === 'R'
+          ? 1
+          : 0;
+      instructions.index++;
+      for (let [i, u] of nodes.entries()) {
+        const uIndex = this.nodeIndex.get(u)!;
+        const v = this.adjacent[uIndex][next];
+        if (v[2] != 'Z') goalReached = false;
+        nodes[i] = v;
+      }
+      //console.log(nodes);
+    }
+
+    return instructions.index;
+  }
+
   override partTwo(input: string): string {
-    throw new Error('Method not implemented.');
+    let lines = input.split('\n').filter((line) => line != '');
+    const instructions = lines[0];
+    lines = lines.slice(1);
+    let index = 0;
+    let startNodes: string[] = [];
+    for (let line of lines) {
+      const [node, neighbors] = line.split(' = ');
+      this.nodeIndex.set(node, index++);
+      const [neighborLeft, neighborRight] = neighbors
+        .slice(1, neighbors.length - 1)
+        .split(', ');
+      this.adjacent.push([neighborLeft, neighborRight]);
+      if (node[2] == 'A') startNodes.push(node);
+    }
+    console.log(startNodes);
+    return this.ghostSearch(startNodes, {
+      directions: instructions,
+      index: 0,
+    }).toString();
   }
 }

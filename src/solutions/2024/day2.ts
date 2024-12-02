ff990e665a845d176a7df9a3e4c11e6d06f276c7
Nicolas Guasch <nic.guasch@gmail.com>
2024-12-02 07:50:39 -0300

    Polish day 2 2024

--- src/solutions/2024/day2.ts
+++ src/solutions/2024/day2.ts
@@ -2,46 +2,39 @@ import { Solution } from '../../app/domains/solver/models/solution';
 
 export class Y2024Day2 extends Solution {
   private checkSafety(levels: number[]): boolean {
-    let minDifference = levels[1] - levels[0],
-      maxDifference = minDifference;
-    for (let i = 2; i < levels.length; i++) {
-      minDifference = Math.min(minDifference, levels[i] - levels[i - 1]);
-      maxDifference = Math.max(maxDifference, levels[i] - levels[i - 1]);
-    }
     return (
-      (minDifference >= 1 && maxDifference <= 3) ||
-      (minDifference >= -3 && maxDifference <= -1)
+      levels
+        .slice(1)
+        .every(
+          (level, i) => 1 <= level - levels[i] && level - levels[i] <= 3,
+        ) ||
+      levels
+        .slice(1)
+        .every((level, i) => -3 <= level - levels[i] && level - levels[i] <= -1)
     );
   }
 
   override partOne(input: string): string {
-    let safeReports = 0;
-    for (let line of input.trim().split(/\n/)) {
-      let levels = line.split(/\s/).map(Number);
-      if (this.checkSafety(levels)) {
-        safeReports++;
-      }
-    }
-    return safeReports.toString();
+    let reports = input
+      .trim()
+      .split(/\n/)
+      .map((line) => line.split(/\s/).map(Number));
+    let safeReports = reports.filter((report) => this.checkSafety(report));
+    return safeReports.length.toString();
   }
 
   problemDampener(levels: number[]): boolean {
-    for (let i = 0; i < levels.length; i++) {
-      let dampened = [...levels];
-      dampened.splice(i, 1);
-      if (this.checkSafety(dampened)) return true;
-    }
-    return false;
+    return levels.some((_, i) => this.checkSafety(levels.toSpliced(i, 1)));
   }
 
   override partTwo(input: string): string {
-    let safeReports = 0;
-    for (let line of input.trim().split(/\n/)) {
-      let levels = line.split(/\s/).map(Number);
-      if (this.checkSafety(levels) || this.problemDampener(levels)) {
-        safeReports++;
-      }
-    }
-    return safeReports.toString();
+    let reports = input
+      .trim()
+      .split(/\n/)
+      .map((line) => line.split(/\s/).map(Number));
+    let safeReports = reports.filter(
+      (report) => this.checkSafety(report) || this.problemDampener(report),
+    );
+    return safeReports.length.toString();
   }
 }

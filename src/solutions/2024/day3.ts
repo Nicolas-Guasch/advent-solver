4d7ea91638bfc49759d45b2e95a41d1712f2eb14
Nicolas Guasch <nic.guasch@gmail.com>
2024-12-03 12:49:31 -0300

    Improve day 3

--- src/solutions/2024/day3.ts
+++ src/solutions/2024/day3.ts
@@ -2,7 +2,7 @@ import { Solution } from '../../app/domains/solver/models/solution';
 
 export class Y2024Day3 extends Solution {
   override partOne(input: string): string {
-    const mulPattern: RegExp = new RegExp(/mul\((\d+),(\d+)\)/g);
+    const mulPattern: RegExp = new RegExp(/mul\((\d{1,3}),(\d{1,3})\)/g);
     let result = 0;
     for (let mul of input.matchAll(mulPattern)) {
       result += parseInt(mul[1]) * parseInt(mul[2]);
@@ -13,9 +13,10 @@ export class Y2024Day3 extends Solution {
     const mulPattern: RegExp = new RegExp(/mul\((\d+),(\d+)\)/g);
     let enabledMuls = '';
     let result = 0;
-    for (let doChunk of input.split('do()')) {
-      enabledMuls += doChunk.split("don't()")[0];
-    }
+    enabledMuls = input
+      .split('do()')
+      .map((doChunk) => doChunk.split("don't()")[0])
+      .join();
     for (let mul of enabledMuls.matchAll(mulPattern)) {
       result += parseInt(mul[1]) * parseInt(mul[2]);
     }
